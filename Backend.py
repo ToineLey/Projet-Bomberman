2760aa68186f7cd24d7ba1a5a2943fec548a194c
ToineLey <toineleydert@gmail.com>
2023-12-11 15:24:34 +0100

    back

--- Backend.py
+++ Backend.py
@@ -1,6 +1,43 @@
 class Grille:
-    def __init__(self):
-        self.cases = [[i for i in range(100,113)] for _ in range(100,111)]
-        pass
-    def create(self):
-        pass
+    def __init__(self, l: int, h: int):
+        self.hauteur = h
+        self.largeur = l
+        self.cases = [[j for j in range(l)] for i in range(h)]
+    def _remplir(self):
+        def ot():
+            return (
+                n not in (0,self.largeur-1) and
+                a not in (0,self.hauteur-1) and
+                a%2!=0 and
+                n%2!=0
+            )
+        def at():
+            return (
+                a in (0,1) and
+                n in (1,self.largeur-2,self.largeur-1)
+            )
+        for a in range(len(self.cases)):
+            for n in self.cases[a]:
+                if ot():
+                    self.cases[a][n]=Case(a,n,2)
+                elif at():
+                    self.cases[a][n]=Case(a,n,0)
+                else:
+                    self.cases[a][n]=Case(a,n,1)
+
+
+class Case:
+    def __init__(self, x, y, t):
+        self.x = x
+        self.y = y
+        self.terrain = t
+        self.bomb = 3
+        self.explode = 4
+        self.player = 5
+    def __str__(self):
+        return self.terrain
+    
+
+a=Grille(13,11)
+a._remplir()
+print(a.cases)
